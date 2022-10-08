870378c0256631478afa4af7e9d131074d0d0aeb
shaikh-debian <skmudassirahmed@gmail.com>
2022-10-08 12:08:08 +0530

    Login,  Logout, Return users list

--- social/models.py
+++ social/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class  CustomAccountManager(BaseUserManager):
+    def create_user(self, email, username,  password, first_name, last_name, **other_fields):
+    
+        if not email:
+            raise ValueError("Users must have email address")
+        if not username:
+            raise ValueError("Users must have an username")    
+        if not first_name:
+            raise ValueError("Users must have a first_name")
+        if not last_name:
+            raise ValueError("Users must have a last_name")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password, first_name, last_name, **other_fields):
+        
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser has to be assigned is_staff = True')
+        
+        if other_fields.get('is_superuser') is  not True:
+            raise ValueError('Superuser has to be assigned is_superuser = True')
+
+        return self.create_user(email, username, password, first_name, last_name, **other_fields)
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    password = models.CharField(max_length=250)
+    
+    first_name = models.CharField(max_length=100,null=False, blank=False)
+    last_name = models.CharField(max_length=100,null=False, blank=False)
+    following = models.ManyToManyField("social.User", blank=True, related_name="followed_by")
+    followers = models.ManyToManyField("social.User", blank=True, related_name="follows")
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    objects = CustomAccountManager()
+
+    def __str__(self):
+        return self.username
--- social/views.py
+++ social/views.py
@@ -1,66 +1,57 @@
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+from curses import use_default_colors
 from django.views import View
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model, authenticate
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .models import *
 
-class Login(TemplateView):
+class Login(View):
+    def get(self, request):
+        if 'user_id' in request.session:
+            return HttpResponseRedirect('')
+        
+        return render(request, 'social/login.html', {
+        })
+
     def post(self, request, *args, **kwargs):
         email = request.POST['email']
         password = request.POST['password']
         
-        user = authenticate(username=email, password=password)
+        user = authenticate(email=email, password=password)
         if user is not None:
-            print("sdfsd")
-            request.session["user_id"] = user.id
-            return HttpResponseRedirect('')
+            request.session['user_id'] = user.id
+            return HttpResponseRedirect('/')
         else:
             return HttpResponseRedirect('login')
 
-    template_name = "social/login.html"
-
-class Register(TemplateView):
+class Register(View):
     def post(self, request, *args, **kwargs):
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+        username = email.split('@')[0]
+        print(first_name, last_name)
+        get_user_model().objects.create_user(email, username, password, first_name, last_name)
         
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
         return HttpResponseRedirect('login')	
 
 class Home(View):
     def get(self, request):
-        if "user_id" not in request.session:
-            return HttpResponseRedirect('login')
-
         # Get all users list except self , render it
         # Get all the post of followed users
-        
-        return HttpResponse("asdfsd")
+
+        user_id = request.session['user_id']
+        users = User.objects.exclude(id=user_id).exclude(username='admin')
+        print(users)
+        return render(request, 'social/index.html', {
+        })
 
 class Logout(View):
     def get(self, request):
         try:
-            del request.session["user_id"]
-            return HttpResponseRedirect("login")
-        except KeyError:
-            pass
-        return HttpResponseRedirect("")
-# class CreateView(View):
-# 	def get(self, request):
-# 		s = "{'key':'value'}"
-# 		url = pyqrcode.create(s)
-# 		buffer = io.BytesIO()
-# 		url.svg(buffer, scale=10)
-# 		return HttpResponse(buffer.getvalue())
-
-# class ScanView(TemplateView):
-# 	def post(self, request, *args, **kwargs):
-# 		return HttpResponse(f"{request.POST['fname']} {request.POST['lname']}")
-
-
-# 	template_name = "attendance/index.html"
-
+            del request.session['user_id']
+            return HttpResponseRedirect('login')
+        except KeyError as e:
+            print(e)
+            return HttpResponseRedirect('/')
